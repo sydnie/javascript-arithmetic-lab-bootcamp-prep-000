95b0969b8b877b284c6deaf6c59eb9e5b591ed61
Sydnie T <sydnietom@gmail.com>
2018-03-31 06:19:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function add(a, b) {
-  return a+b
+  return a+b;
 }
 
 function subtract(a, b) {
-  return a-b
+  return a-b;
 }
 
 function multiply(a, b) {
-  return a*b
+  return a*b;
 }
 
 function divide(a, b) {
-  return a/b 
+  return a/b;
 }
 
 function inc(a) {
-  return ++a
+  return ++a;
 }
 
 function dec(a) {
-  return --a
+  return --a;
 }
 
 function makeInt(a) {
-  return parseInt(a, 10)
+  return parseInt(a, 10);
 } else {
-  return NaN
-  }
+  return NaN;
+}
 
 
