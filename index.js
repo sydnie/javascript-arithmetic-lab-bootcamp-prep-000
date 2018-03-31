f7357783d18a90f0fc08c49208ecb32924f6943b
Sydnie T <sydnietom@gmail.com>
2018-03-31 06:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function makeInt(a) {
   }
 }
   
+  
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
